7877333af92a8954822f21a047207a24e696472d
jjindol <andy15948@naver.com>
2023-08-15 21:48:12 +0900

    230815 commit

--- src/test/java/com/shop/controller/MemberControllerTest.java
+++ src/test/java/com/shop/controller/MemberControllerTest.java
@@ -1,8 +1,14 @@
 package com.shop.controller;
 
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -10,4 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @TestPropertySource(locations = "classpath: application-test.properties")
 public class MemberControllerTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc; // 테스트에 필요한 기능만 갖는 가짜 객체
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password) { // 회원을 등록하는 메서드
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 양천구 목동");
+        memberFormDto.setPassword(password);
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
 }
