1048514cbd016792413ad354f1ea4b193cd96bb7
jjindol <andy15948@naver.com>
2023-08-18 18:11:49 +0900

    230818 commit

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -28,26 +28,9 @@ public class MemberController {
         return "member/memberForm";
     }
 
-    @PostMapping(value = "new")
-    public String memberForm(MemberFormDto memberFormDto) {
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
-        return "redirect:/";
-    }
-
-    @GetMapping(value = "/login")
-    public String loginMember() {
-        return "/member/memberLoginForm";
-    }
-
-    @GetMapping(value = "/login/error")
-    public String loginError(Model model) {
-        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-        return "member/memberLoginForm";
-    }
-
     @PostMapping(value = "/new")
     public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+
         if (bindingResult.hasErrors()) {
             return "member/memberForm";
         }
@@ -59,6 +42,18 @@ public class MemberController {
             model.addAttribute("errorMessage", e.getMessage());
             return "member/memberForm";
         }
+
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginMember() {
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
     }
 }
--- src/test/java/com/shop/controller/MemberControllerTest.java
+++ src/test/java/com/shop/controller/MemberControllerTest.java
@@ -13,13 +13,12 @@ import org.springframework.security.test.web.servlet.response.SecurityMockMvcRes
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@TestPropertySource(locations = "classpath: application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties") // 공백 주의
 public class MemberControllerTest {
 
     @Autowired
@@ -31,27 +30,25 @@ public class MemberControllerTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public Member createMember(String email, String password) { // 회원을 등록하는 메서드
+    public Member createMember(String email, String password){
         MemberFormDto memberFormDto = new MemberFormDto();
         memberFormDto.setEmail(email);
         memberFormDto.setName("홍길동");
-        memberFormDto.setAddress("서울시 양천구 목동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
         memberFormDto.setPassword(password);
-
         Member member = Member.createMember(memberFormDto, passwordEncoder);
         return memberService.saveMember(member);
     }
 
     @Test
     @DisplayName("로그인 성공 테스트")
-    public void loginSuccessTest() throws Exception {
+    public void loginSuccessTest() throws Exception{
         String email = "test@email.com";
         String password = "1234";
         this.createMember(email, password);
-
         mockMvc.perform(formLogin().userParameter("email")
-                .loginProcessingUrl("/members/login")
-                .user(email).password(password))
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password(password))
                 .andExpect(SecurityMockMvcResultMatchers.authenticated());
     }
 }
