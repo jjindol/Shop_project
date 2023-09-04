2cd9da833a7a67706b456b60520b63f293fc84df
jjindol <andy15948@naver.com>
2023-09-04 14:33:28 +0900

    230904 commit

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.shop.config;
 
-
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,26 +11,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
 @Configuration
-public class SecurityConfig { // WebSecurityConfigurerAdapter 상속 불가(지원 중단)
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    MemberService memberService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // http 요청에 대한 보안 설정
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage("/members/login") // 로그인 페이지 URL 설정
-                .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL
-                .usernameParameter("email") // 로그인 시 사용할 파라미터 이름 email
-                .failureUrl("/members/login/error") // 로그인 실패 시 이동할 URL
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
                 .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 URL
-                .logoutSuccessUrl("/"); // 로그아웃 성공 시 이동할 URL
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+        ;
+
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                .antMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN") // ADMIN Role 일 경우에만 접근 가능
+                .anyRequest().authenticated()
+        ; // static 하위 파일은 인증 무시
+
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ; // 인증x 사용자가 리소스에 접근할 때 수행되는 핸들러
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // 비밀번호를 암호화하여 저장
+        return new BCryptPasswordEncoder();
     }
 }
