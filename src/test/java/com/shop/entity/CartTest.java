86a097e263bd49e4354ef42de6d0bf198d386bb0
jjindol <andy15948@naver.com>
2023-09-07 23:37:14 +0900

    230907 commit

---
+++ src/test/java/com/shop/entity/CartTest.java
@@ -0,0 +1,12 @@
+package com.shop.entity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class CartTest {
+
+}
