6f0f11bc7987f3979d96f82429c3ea3e62b6860b
jjindol <andy15948@naver.com>
2023-09-08 21:02:50 +0900

    230908 commit

--- src/test/java/com/shop/entity/CartTest.java
+++ src/test/java/com/shop/entity/CartTest.java
@@ -1,12 +1,46 @@
 package com.shop.entity;
 
+import com.shop.dto.MemberFormDto;
+import com.shop.repository.CartRepository;
+import com.shop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static com.shop.entity.Member.createMember;
 
 @SpringBootTest
 @Transactional
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class CartTest {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member CreateMember() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+        return createMember(memberFormDto, passwordEncoder);
+    }
+
 
 }
