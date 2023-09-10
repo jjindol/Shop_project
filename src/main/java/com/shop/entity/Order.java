9c2cf15e8a8b6a5aa285b52eac268ff3d60ae823
jjindol <andy15948@naver.com>
2023-09-10 23:28:30 +0900

    230910 commit

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -1,10 +1,14 @@
 package com.shop.entity;
 
+import com.shop.constant.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -15,5 +19,21 @@ public class Order {
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문 상태
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
 
 }
