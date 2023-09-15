56df64ffd7592f964b49317bc50fd4820dabcf9c
jjindol <andy15948@naver.com>
2023-09-15 16:48:48 +0900

    230915 commit

--- src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -14,10 +14,10 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
-        if ("XMLHttpRequest".equals(request.getHeader("x-requested-with"))) {
+        if ("XMLHttpRequest".equals(request.getHeader("x-requested-with"))) { // "x-requested-with" 헤더 값 확인
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
         } else {
-            response.sendRedirect("/members/login");
+            response.sendRedirect("/members/login"); // Ajax 요청이 아닌 경우 리다이렉트
         }
     }
 }
