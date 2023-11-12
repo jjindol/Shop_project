f8f6522b54161de15d3836ea051ace5d024ab641
jjindol <andy15948@naver.com>
2023-11-12 22:24:55 +0900

    231112 commit

--- src/main/java/com/shop/constant/ItemSellStatus.java
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -3,5 +3,3 @@ package com.shop.constant;
 public enum ItemSellStatus {
     SELL, SOLD_OUT
 }
-
-// 상품이 현재 판매 중인지, 품절 상태인지 나타냄
--- src/main/java/com/shop/constant/Role.java
+++ src/main/java/com/shop/constant/Role.java
@@ -3,4 +3,4 @@ package com.shop.constant;
 public enum Role {
     USER, ADMIN
 }
-// 사용자의 권한이 user 인지 admin 인지 결정
+// 권한이 user 인지 admin 인지 결정
