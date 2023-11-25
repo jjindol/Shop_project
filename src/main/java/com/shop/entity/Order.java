c2e9ddd88a83c3e68599c73dcc1a05bec2f16b1e
jjindol <andy15948@naver.com>
2023-11-25 23:38:09 +0900

    231125 commit

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -1,7 +1,16 @@
 package com.shop.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "orders")
 public class Order {
-
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
 
 }
